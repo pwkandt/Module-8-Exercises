33b41add77ce93e59e3bcdb5013ca447ab7e87fa
pwkandt <123347213+pwkandt@users.noreply.github.com>
2023-04-04 03:24:32 -0400

    Create ex08_03.py

---
+++ ex08_03/ex08_03.py
@@ -0,0 +1,8 @@
+fhand = open('test.txt')
+count = 0
+for line in fhand:
+    words = line.split()
+    # print('Debug:', words)
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    print(words[2])
